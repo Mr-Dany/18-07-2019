9e809eb98ee8e20a3051fed2ff90099ebd31207c
Mr-Dany <toshibaqosmiox75@hotmail.com>
2019-07-19 11:23:42 -0500

    tarea de clases campo  obligatorio pero para todos los campos

--- myjavascript.js
+++ myjavascript.js
@@ -1,11 +1,37 @@
 var contador=0;
 $(document).ready(function(){
 	$('#agregar').click(function(){
-		$('#prueba').css({'color':'red','font-size':'50px'})
-		if($('#nombre').val()=="" && $('#apellido').val()=="" && $('#cedula').val()==""  ){
-			$('#prueba').html("Los campos son obligatotios");
+		if($('#nombre').val()==""){
+			$('#mnombre').css({'color':'red','font-size':'10px'})
+			$('#mnombre').html("Los campos son obligatotios");
 		}
+		if($('#apellido').val()==""){
+			$('#mapellido').css({'color':'red','font-size':'10px'})
+			$('#mapellido').html("Los campos son obligatotios");
+		}
+		if($('#cedula').val()==""){
+			$('#mcedula').css({'color':'red','font-size':'10px'})
+			$('#mcedula').html("Los campos son obligatotios");
+		}
+
 		
 
 	});
 });
+
+
+
+
+
+
+// var contador=0;
+// $(document).ready(function(){
+// 	$('#agregar').click(function(){
+// 		$('#prueba').css({'color':'red','font-size':'50px'})
+// 		if($('#nombre').val()=="" && $('#apellido').val()=="" && $('#cedula').val()==""  ){
+// 			$('#prueba').html("Los campos son obligatotios");
+// 		}
+		
+
+// 	});
+// });
