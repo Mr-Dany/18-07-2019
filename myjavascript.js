3c5cdd85dec763952318c5497167e91e9d40564b
Mr-Dany <toshibaqosmiox75@hotmail.com>
2019-07-19 08:15:09 -0500

    formulario con query

--- myjavascript.js
+++ myjavascript.js
@@ -1,7 +1,11 @@
 var contador=0;
 $(document).ready(function(){
-	
 	$('#agregar').click(function(){
-		$('#prueba').html(contador++);
+		$('#prueba').css({'color':'red','font-size':'50px'})
+		if($('#nombre').val()=="" && $('#apellido').val()=="" && $('#cedula').val()==""  ){
+			$('#prueba').html("Los campos son obligatotios");
+		}
+		
+
 	});
 });
